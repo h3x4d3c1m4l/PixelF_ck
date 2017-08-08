b02d4b9965da6ba67158c167264b7de252ed71cb
Sander in 't Hout <sander@inthout.eu>
2017-08-08 03:25:53 +0200

    testing testing

---
+++ PixelF_ck/Pixelflut.cs
@@ -0,0 +1,101 @@
+﻿using ImageSharp;
+using ImageSharp.PixelFormats;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixelF_ck
+{
+    class Pixelflut : IDisposable
+    {
+        private TcpClient _client;
+
+        private StreamReader _reader;
+
+        private StreamWriter _writer;
+
+        private string _hostname;
+
+        private ushort _port;
+
+        public Pixelflut(string pHostname, ushort pPort)
+        {
+            _hostname = pHostname;
+            _port = pPort;
+        }
+
+        public async Task Connect()
+        {
+            _client = new TcpClient();
+            await _client.ConnectAsync(_hostname, _port);
+            _writer = new StreamWriter(_client.GetStream(), Encoding.ASCII);
+            _reader = new StreamReader(_client.GetStream(), Encoding.ASCII);
+        }
+
+        public async Task<(int X, int Y)> GetResolutionAsync()
+        {
+            await _writer.WriteAsync("SIZE\n");
+            await _writer.FlushAsync();
+            while (!_client.GetStream().DataAvailable)
+                await Task.Delay(10);
+            var line = _reader.ReadLine(); // returns something like "SIZE 1024 768"
+            _reader.DiscardBufferedData(); // clears any additional CR/LF
+            var split = line.Split(' ');
+            return (int.Parse(split[1]), int.Parse(split[2]));
+        }
+
+        public async Task SendImage(Rgba32[] pPixels, int pHorizontalPixels)
+        {
+            var px = 0;
+ 
+            for (var y = 0; y < (pPixels.Length / pHorizontalPixels); y++)
+            {
+                for (var x = 0; x < pHorizontalPixels; x++)
+                {
+                    await _writer.WriteAsync($"PX {x} {y} {pPixels[px].ToHex().Substring(0, 6)}\n");
+                    px++;
+                }
+            }
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _reader?.Dispose();
+                    _writer?.Dispose();
+                    _client?.Dispose();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~Pixelflut() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
---
+++ PixelF_ck/Program.cs
@@ -0,0 +1,90 @@
+﻿using ImageSharp;
+using PixelF_ck;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static string _file;
+
+        private static string _hostname;
+
+        private static ushort _ports;
+
+        private static int _threads;
+
+        private static void Main(string[] args)
+        {
+            _hostname = args[0];
+            _ports = ushort.Parse(args[1]);
+            _file = args[2];
+            _threads = int.Parse(args[3]);
+
+            Test().Wait();
+            Console.In.Read();
+        }
+
+        private static async Task Test()
+        {
+            using (var pf = new Pixelflut(_hostname, _ports))
+            {
+                // connect and get resolution
+                await pf.Connect();
+                var res = await pf.GetResolutionAsync();
+                Console.WriteLine($"Connected, screen res: {res.X}x{res.Y}");
+
+                // decode image and resize to screen res
+                Rgba32[] pixels;
+                using (Image<Rgba32> image = Image.Load(_file))
+                {
+                    Console.WriteLine($"Img size: {image.Width}x{image.Height}");
+                    // https://stackoverflow.com/questions/1940581/c-sharp-image-resizing-to-different-size-while-preserving-aspect-ratio
+                    double ratioX = res.X / (double)image.Width;
+                    double ratioY = res.Y / (double)image.Height;
+                    double ratio = ratioX < ratioY ? ratioX : ratioY;
+                    int newHeight = Convert.ToInt32(image.Height * ratio);
+                    int newWidth = Convert.ToInt32(image.Width * ratio);
+                    image.Resize(newWidth, newHeight);
+                    Console.WriteLine($"Image resized to: {image.Width}x{image.Height}");
+                    image.Pad(res.X, res.Y);
+                    pixels = image.Pixels.ToArray();
+                    Console.WriteLine($"Image pad to: {image.Width}x{image.Height}");
+                    image.SaveAsPng(File.OpenWrite(@"C:\temp\test2.png"));
+                }
+
+                for (var i = 0; i < _threads; i++)
+                {
+                    var t = new Thread(() =>
+                    {
+                        using (var tpf = new Pixelflut(_hostname, _ports))
+                        {
+                            while (true)
+                            {
+                                try
+                                {
+                                    tpf.Connect().Wait();
+                                    while (true)
+                                    {
+                                        tpf.SendImage(pixels, res.X).Wait();
+                                        Console.Out.Write('*');
+                                    }
+                                }
+                                catch (Exception ex)
+                                {
+                                    //
+                                    Console.Out.Write('X');
+                                }
+                            }
+                        }
+                    });
+                    t.Start();
+                }
+            }
+        }
+    }
+}
